839261aca78cc17563211a80dc5d339d68b65915
“tianchengcheng” <tian.chengcheng@vpclub.cn>
2020-04-04 17:06:27 +0800

    提交dist

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production' ?
+		'/my-project/' // github pages 打包路径
+		:
+		'/',
 	outputDir: 'dist', //build输出目录
 	assetsDir: 'assets', //静态资源目录（js, css, image）
 	lintOnSave: false, //是否开启eslint
